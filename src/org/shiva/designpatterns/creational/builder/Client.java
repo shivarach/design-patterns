d4bf475ceaa866dad49235d88d300887b9eda1b4
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-12-25 23:03:40 +0530

    Update comments in builder design pattern

--- src/org/shiva/designpatterns/creational/builder/Client.java
+++ src/org/shiva/designpatterns/creational/builder/Client.java
@@ -3,6 +3,8 @@ package org.shiva.designpatterns.creational.builder;
 import org.shiva.designpatterns.creational.builder.model.Person;
 
 public class Client {
+   /*There is other variation of builder pattern also check
+    https://springframework.guru/gang-of-four-design-patterns/builder-pattern/*/
     public static void main(String[] args) {
         final Person person = new PersonBuilder()
                 .setFirstName("Siva")
